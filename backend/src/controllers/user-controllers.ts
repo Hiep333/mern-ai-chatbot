7f17be30c04abe2fd098259f0d980e5113183fd9
Hiep <tothanhhiep.work@gmail.com>
2024-05-29 13:33:45 +0700

    Creating user sigup Post route to store users

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -17,3 +17,20 @@ export const getAllUsers = async (
   }
 };
 
+export const userSignUp = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      //user signup
+      const { name, email, password } = req.body;
+      const hashedPassword = await hash(password, 10);
+      const user = new User({ name, email, password: hashedPassword });
+      await user.save();
+      return res.status(200).json({ message: "OK", id: user._id.toString() });
+    } catch (error) {
+      console.log(error);
+      return res.status(200).json({ message: "Error", cause: error.message });
+    }
+  };
