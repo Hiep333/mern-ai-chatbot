d9adfb0ee8c4444f8895d3cd78992802f2e61b7f
Hiep <tothanhhiep.work@gmail.com>
2024-05-29 14:16:46 +0700

    setting HTTP only cookies with Cookie-parser

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import User from "../models/User.js";
 import { hash, compare } from "bcrypt";
 import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 
 export const getAllUsers = async (
   req: Request,
@@ -32,6 +33,25 @@ export const userSignUp = async (
     const hashedPassword = await hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
+    // create token and store cookie
+
+    res.clearCookie(COOKIE_NAME, {
+        httpOnly: true,
+        domain: "localhost",
+        signed: true,
+        path: "/",
+      });
+  
+      const token = createToken(user._id.toString(), user.email, "7d");
+      const expires = new Date();
+      expires.setDate(expires.getDate() + 7);
+      res.cookie(COOKIE_NAME, token, {
+        path: "/",
+        domain: "localhost",
+        expires,
+        httpOnly: true,
+        signed: true,
+      });
     return res.status(201).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
     console.log(error);
@@ -40,28 +60,46 @@ export const userSignUp = async (
 };
 
 export const userLogin = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      //user login
-      const { email, password } = req.body;
-      //check user exists or not
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.status(401).send("User not registered");
-      }
-      const isPasswordCorrect = await compare(password, user.password);
-      if (!isPasswordCorrect) {
-        return res.status(403).send("Incorrect Password");
-      }
-      
-      const token = createToken(user._id.toString(), user.email, "7d");
-
-      return res.status(200).json({ message: "OK", id: user._id.toString() });
-    } catch (error) {
-      console.log(error);
-      return res.status(200).json({ message: "Error", cause: error.message });
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    //user login
+    const { email, password } = req.body;
+    //check user exists or not
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).send("User not registered");
     }
-  };
+    const isPasswordCorrect = await compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(403).send("Incorrect Password");
+    }
+
+    // create token and store cookie
+
+    res.clearCookie(COOKIE_NAME, {
+      httpOnly: true,
+      domain: "localhost",
+      signed: true,
+      path: "/",
+    });
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
+    return res.status(200).json({ message: "OK", id: user._id.toString() });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "Error", cause: error.message });
+  }
+};
