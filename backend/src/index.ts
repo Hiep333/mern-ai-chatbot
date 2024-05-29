80d900dfa4733e9ea402f3da02bf81e775a8717e
Hiep <tothanhhiep.work@gmail.com>
2024-05-29 12:58:29 +0700

    setup MongoDB Database connection

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+
+//connections and listeners
+const PORT = process.env.PORT || 3033;
+connectToDatabase().then(() => {
+  app.listen(PORT, () => console.log(`Server Open & Connected To Database at ${PORT}`));
+})
+.catch((error) => console.log(error.message));
+
