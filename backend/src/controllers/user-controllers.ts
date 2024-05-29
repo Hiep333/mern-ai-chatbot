1e3db1ed70cc8ce7c4e555d8c21464f2deefefd1
Hiep <tothanhhiep.work@gmail.com>
2024-05-29 13:25:54 +0700

    Creating first GET Request to get all users

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,3 +1,19 @@
-export const getAllUsers = async () => {
-   
-  };
+import { Request, Response, NextFunction } from "express";
+import User from "../models/User.js";
+import { hash } from "bcrypt";
+
+export const getAllUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    //get all users
+    const users = await User.find();
+    return res.status(200).json({ message: "OK", users });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "Error", cause: error.message });
+  }
+};
+
