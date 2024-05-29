222ccff1440a3bf221abf170aa96e57ea01e76a9
Hiep <tothanhhiep.work@gmail.com>
2024-05-29 14:05:42 +0700

    Implementing JWT Tokens

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/User.js";
 import { hash, compare } from "bcrypt";
+import { createToken } from "../utils/token-manager.js";
 
 export const getAllUsers = async (
   req: Request,
@@ -55,6 +56,9 @@ export const userLogin = async (
       if (!isPasswordCorrect) {
         return res.status(403).send("Incorrect Password");
       }
+      
+      const token = createToken(user._id.toString(), user.email, "7d");
+
       return res.status(200).json({ message: "OK", id: user._id.toString() });
     } catch (error) {
       console.log(error);
